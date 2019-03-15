69d1cfa018c00ed3c1e25cdb2e496a64b4adebcb
Duan <1172215944@qq.com>
2019-03-15 13:37:02 +0800

    Update 1004 成绩排名.c

--- 1004 成绩排名.c
+++ 1004 成绩排名.c
@@ -1,3 +1,9 @@
+/* name id两个变量是定义的二维数组。name[i] id[i]都是一个一维数组，
+一维数组名可以直接当首地址的指针用，但score本身就是一维数组，score[i]就是一个元素
+，如果你要给这个元素赋值就要取其地址。
+*/
+/* 也可以写成score+i，表示给第i个元素赋值，score是数组名做指针+i表示指针往后跳i位，一样可以不用&符号
+*/
 #include<stdio.h>
 
 int main(){
@@ -9,7 +15,7 @@ int main(){
 	scanf("%d",&n);
 	
 	for(int i =0;i<n;i++){
-		scanf("%s %s %d",name[i],id[i],&score[i]);   //这里需要用 & 有疑问
+		scanf("%s %s %d",name[i],id[i],&score[i]);
 		if(max<score[i]){
 			max = score[i];
 			maxIndex = i;
